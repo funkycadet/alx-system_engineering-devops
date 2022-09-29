d1d6dc280edb022606f2a5708c791eff0dbef39a
smith2eric <ericsmithsonian2@gmail.com>
2022-09-29 12:31:40 +0100

    [fix]: resolved errors in puppet file

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,19 @@
-# Installs a Nginx server with custome HTTP header
+# Installs an Nginx server with custom HTTP header
 
 exec {'update':
-  provider => shell,
-  command  => 'sudo apt-get -y update',
-  before   => Exec['install Nginx'],
+  command  => '/usr/bin/apt update',
 }
 
 exec {'install Nginx':
-  provider => shell,
   command  => 'sudo apt-get -y install nginx',
   before   => Exec['add_header'],
 }
 
 exec { 'add_header':
-  provider    => shell,
-  command     => 'sudo sed -i "25i\\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default,
+  command     => 'sed -i "25i\\\tadd_header X-Served-By \$hostname;" /etc/nginx/sites-available/default,
   before      => Exec['restart Nginx'],
 }
 
 exec { 'restart Nginx':
-  provider => shell,
   command  => 'sudo service nginx restart',
 }
